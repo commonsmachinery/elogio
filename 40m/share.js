001fc51fe0b29bd1ca40b9fd97139a0f886007bc
Jonas Oberg <jonas@ffkp.se>
2015-01-30 21:30:23 +0100

    Bugfix #

--- 40m/share.js
+++ 40m/share.js
@@ -23,7 +23,7 @@ $(function() {
     
     $('.twitter').on('click', function() {
         var loc = encodeURIComponent('http://elog.io/40m'),
-                title = "Credit where credit is due, join the #attributionrevolution — ",
+                title = "Credit where credit is due, join the \#attributionrevolution — ",§
                 w = 580, h = 300,
                 left = (screen.width/2)-(w/2),
                 top = (screen.height/2)-(h/2);
