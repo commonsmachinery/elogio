5ae693a99963d8dde8a202121e8501cb85a6d0e3
Jonas Oberg <jonas@ffkp.se>
2015-02-03 21:24:26 +0100

    Provenance :-)

--- 40m/share.js
+++ 40m/share.js
@@ -23,7 +23,7 @@ $(function() {
     
     $('.twitter').on('click', function() {
         var loc = encodeURIComponent('http://elog.io/40m/'),
-                title = "I believe in giving and receiving credit for photographs online. Do you? Join the %23attributionrevolution - ",
+                title = "Help support Elog.io and digital provenance for photographs online! - ",
                 w = 580, h = 300,
                 left = (screen.width/2)-(w/2),
                 top = (screen.height/2)-(h/2);
